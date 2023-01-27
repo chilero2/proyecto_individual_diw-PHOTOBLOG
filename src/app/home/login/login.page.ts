de9f92b30168b147bb0f3c095f5e0b5d7f16194d
emilio <emilioblasco@icloud.com>
2023-01-27 13:34:09 +0100

    add form

--- src/app/home/login/login.page.ts
+++ src/app/home/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -8,8 +9,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class LoginPage implements OnInit {
   @Input() header: boolean
 
-  constructor() {
+  formularioLogin: FormGroup
+
+  constructor(public fb: FormBuilder) {
     this.header = false
+    this.formularioLogin = this.fb.group({
+      'email': new FormControl("", Validators.required), 
+      'password' : new FormControl("", Validators.required)
+    })
   }
 
   ngOnInit() {
